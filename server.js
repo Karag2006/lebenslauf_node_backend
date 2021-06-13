f0c16e6fc86cd3540e5ebb895893336a8ad1daef
Karag <karag2006@gmx.net>
2021-06-13 11:47:43 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+/*  Requires:
+*
+*   MongoDB Database named lebenslauf complete with an admin user for it.
+*   Credentials saved in .env
+*
+*/
+
+const express = require("express");
+const app = express();
+
+app.listen(3000);
